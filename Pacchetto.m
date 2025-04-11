a7d178af3a074b5ff87ad38682a65414aeb82be3
marcomiozza <marco.miozza@studio.unibo.it>
2025-04-11 13:41:10 +0200

    fixing aUI

--- Pacchetto.m
+++ Pacchetto.m
@@ -3,14 +3,14 @@ BeginPackage["Pacchetto`"]
 aUI::usage = "aUI[] mostra:
 - una matrice binaria cliccabile;
 - slider RGB che aggiornano una matrice RGB;
-- visualizzazione della matrice RGB sia come valori che come colori.";
+- visualizzazione della matrice RGB sia come valori che come colori."
 
 Begin["`Private`"]
 
 aUI[] := DynamicModule[
   {
-   mat = ConstantArray[255, {8, 8}],
-   rgbMat = ConstantArray[{255, 255, 255}, {8, 8}],
+   mat = ConstantArray[255, {5, 5}],
+   rgbMat = ConstantArray[{255, 255, 255}, {5, 5}],
    r = 255, g = 255, b = 255
   },
 
@@ -18,7 +18,6 @@ aUI[] := DynamicModule[
 
     (* RIGA 1: matrice 0/255 + ArrayPlot *)
     Row[{
-
       Column[{
         Text["Clicca su una cella per passare da 0 a 255 e viceversa:"],
         Grid[
@@ -29,14 +28,14 @@ aUI[] := DynamicModule[
                 mat[[i, j]] = If[mat[[i, j]] == 0, 255, 0],
                 Appearance -> None,
                 BaseStyle -> {FontSize -> 14, FontWeight -> Bold},
-                Background -> Dynamic[
-                  If[mat[[i, j]] == 0, Gray, LightGray]
-                ]
+                Background -> Dynamic[If[mat[[i, j]] == 0, Gray, LightGray]],
+                ImageSize -> {40, 40}
               ]
             ],
-            {i, 8}, {j, 8}
+            {i, 5}, {j, 5}
           ],
-          Frame -> All
+          Frame -> All,
+          Spacings -> {0, 0}
         ]
       }],
 
@@ -53,68 +52,37 @@ aUI[] := DynamicModule[
           Frame -> True,
           FrameTicks -> None,
           AspectRatio -> 1,
-          ImageSize -> 240
+          ImageSize -> 200
         ]
       ]
-
-    }], (* Fine prima riga *)
+    }],
 
     Spacer[30],
 
-    (* RIGA 2: Slider RGB + Matrice RGB + ArrayPlot RGB *)
+    (* RIGA 2: Matrice RGB + ArrayPlot RGB + SLIDER A DESTRA *)
     Row[{
 
-      (* SLIDERS *)
-      Column[{
-        Style["Regola i canali RGB:", Bold],
-        Slider[
-          Dynamic[r, (r = #) &],
-          {0, 255},
-          Appearance -> "Labeled",
-          ContinuousAction -> False,
-          ImageSize -> 200
-        ],
-        Slider[
-          Dynamic[g, (g = #) &],
-          {0, 255},
-          Appearance -> "Labeled",
-          ContinuousAction -> False,
-          ImageSize -> 200
-        ],
-        Slider[
-          Dynamic[b, (b = #) &],
-          {0, 255},
-          Appearance -> "Labeled",
-          ContinuousAction -> False,
-          ImageSize -> 200
-        ],
-        Button["Applica RGB a tutta la matrice",
-          rgbMat = ConstantArray[{r, g, b}, {8, 8}]
-        ]
-      }],
-
-      Spacer[20],
-
-      (* RGB VALUES GRID (readonly, no wrapping) *)
+      (* RGB VALUES GRID *)
       Dynamic[
         Grid[
           Table[
-            With[{i = i, j = j},
-              Style[
-                StringJoin@Riffle[ToString /@ IntegerPart /@ rgbMat[[i, j]], ", "],
-                FontFamily -> "Courier",
-                FontSize -> 10,
-                FontWeight -> Medium,
-                FixedWidth -> 40
+            With[{val = rgbMat[[i, j]]},
+              Pane[
+                Column[
+                  Style[#, FontFamily -> "Courier", FontSize -> 10, FontWeight -> Medium] & /@ val,
+                  Alignment -> Center,
+                  Spacings -> 0.5
+                ],
+                {50, 50},
+                Alignment -> Center
               ]
             ],
-            {i, 8}, {j, 8}
+            {i, 5}, {j, 5}
           ],
           Frame -> All,
+          Spacings -> {0, 0},
           Alignment -> Center,
-          ColumnWidths -> 7,  (* Set fixed column width for all columns *)
-          RowHeights -> 30,    (* Set fixed row height *)
-          ItemSize -> {1, 1}   (* Prevent resizing based on content *)
+          ItemSize -> All
         ]
       ],
 
@@ -123,25 +91,63 @@ aUI[] := DynamicModule[
       (* COLOR MATRIX *)
       Dynamic[
         ArrayPlot[
-          Map[
-            RGBColor @@ (#/255) &,
-            rgbMat,
-            {2}
-          ],
+          Map[RGBColor @@ (#/255) &, rgbMat, {2}],
           Mesh -> All,
           MeshStyle -> Black,
           Frame -> True,
           FrameTicks -> None,
           AspectRatio -> 1,
-          ImageSize -> 240
+          ImageSize -> 200
         ]
-      ]
+      ],
 
-    }] (* Fine seconda riga *)
+      Spacer[20],
 
-  }] (* Fine Column *)
+      (* SLIDERS A DESTRA SENZA MODIFICABILI *)
+      Column[{
+        Style["Regola i canali RGB:", Bold],
+
+        Row[{
+          Style["R", Bold, Red], 
+          Slider[
+            Dynamic[r, (r = Round[#]) &],
+            {0, 255},
+            ContinuousAction -> False,
+            ImageSize -> 200
+          ]
+        }],
+
+        Row[{
+          Style["G", Bold, Darker[Green]], 
+          Slider[
+            Dynamic[g, (g = Round[#]) &],
+            {0, 255},
+            ContinuousAction -> False,
+            ImageSize -> 200
+          ]
+        }],
+
+        Row[{
+          Style["B", Bold, Blue], 
+          Slider[
+            Dynamic[b, (b = Round[#]) &],
+            {0, 255},
+            ContinuousAction -> False,
+            ImageSize -> 200
+          ]
+        }],
+
+        Spacer[10],
+
+        Button[
+          "Applica RGB a tutta la matrice",
+          rgbMat = ConstantArray[{Round[r], Round[g], Round[b]}, {5, 5}],
+          ImageSize -> 200
+        ]
+      }]
+    }]
+  }]
 ]
 
 End[]
-
 EndPackage[]
